f9ce493cad5a6fb5a6a8874a3c7f74d297c0747b
survikrowa <szymonhrabia60@gmail.com>
2023-12-05 20:51:28 +0100

    feat(native): add expo router

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -3,7 +3,7 @@ const { getDefaultConfig } = require("expo/metro-config");
 const path = require("path");
 
 // Find the workspace root, this can be replaced with `find-yarn-workspace-root`
-const workspaceRoot = path.resolve(__dirname, "../..");
+const workspaceRoot = path.resolve(__dirname, "../../");
 const projectRoot = __dirname;
 
 const config = getDefaultConfig(projectRoot);
