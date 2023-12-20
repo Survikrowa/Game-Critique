6e83f9a8702c79171f76593edf5ce7662d4f006c
survikrowa <szymonhrabia60@gmail.com>
2023-12-20 21:47:54 +0100

    feat(native/api): Begin work on auth0

--- apps/native/app/auth/index.tsx
+++ apps/native/app/auth/index.tsx
@@ -1,11 +1,23 @@
 import * as React from "react";
 import { Text, View } from "react-native";
+import { useAuth0 } from "react-native-auth0";
+import { Button } from "tamagui";
 
 const HomeScreen = () => {
+  const { authorize } = useAuth0();
+
+  const onPress = async () => {
+    try {
+      await authorize();
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <View>
       <Text>Witaj na home screenie</Text>
       <View style={{ flex: 1 }} />
+      <Button onPress={onPress}>Log in</Button>
     </View>
   );
 };
