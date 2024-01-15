0e0504b2978ea1fadf5ead081392b3b8102efb78
survikrowa <szymonhrabia60@gmail.com>
2024-01-15 20:43:08 +0100

    feat(api): Add cloudinary images provider endpoint

---
+++ apps/api/src/modules/images/cloudinary.provider.ts
@@ -0,0 +1,12 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+export const CloudinaryProvider = {
+  provide: 'CLOUDINARY',
+  useFactory: () => {
+    return cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+  },
+};
---
+++ apps/api/src/modules/images/images.controller.ts
@@ -0,0 +1,22 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { ImagesService } from './images.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('images')
+export class ImagesController {
+  constructor(private readonly imagesService: ImagesService) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async upload(@UploadedFile() file: Express.Multer.File) {
+    const { url } = await this.imagesService.uploadImage(file);
+    return {
+      photo_url: url,
+    };
+  }
+}
