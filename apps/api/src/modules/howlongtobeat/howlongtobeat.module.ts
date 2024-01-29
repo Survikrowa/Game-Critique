ba7bd006b960e85b97d4c7abff021bbc2f9eacc8
survikrowa <szymonhrabia60@gmail.com>
2024-01-29 23:05:06 +0100

    feat(api): Cleanup api

--- apps/api/src/modules/howlongtobeat/howlongtobeat.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HowLongToBeatController } from './howlongtobeat.controller';
-import { HowLongToBeatParserModule } from '../howlongtobeat_parser/howlongtobeat_parser.module';
-
-@Module({
-  imports: [HowLongToBeatParserModule],
-  controllers: [HowLongToBeatController],
-})
-export class HowLongToBeatModule {}
