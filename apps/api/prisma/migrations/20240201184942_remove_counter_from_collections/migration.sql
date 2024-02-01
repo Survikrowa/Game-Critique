f0943df974e75808e19ca7a7b6ec0b70956ef14c
survikrowa <szymonhrabia60@gmail.com>
2024-02-01 19:50:21 +0100

    feat(api): Remove counter from table to calculate it directly in code

---
+++ apps/api/prisma/migrations/20240201184942_remove_counter_from_collections/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `counter` on the `collection` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "collection" DROP COLUMN "counter";
