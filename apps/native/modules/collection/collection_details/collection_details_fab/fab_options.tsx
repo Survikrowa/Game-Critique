37360bf83e46cfda3b77f5b76eb27a9bfd298513
survikrowa <szymonhrabia60@gmail.com>
2024-02-04 00:56:03 +0100

    feat(native): Move action names to dictionary

--- apps/native/modules/collection/collection_details/collection_details_fab/fab_options.tsx
+++ apps/native/modules/collection/collection_details/collection_details_fab/fab_options.tsx
@@ -1,9 +1,13 @@
 import { PlusCircle } from "@tamagui/lucide-icons";
 import { IActionProps } from "react-native-floating-action";
 
+export const ACTION_NAMES = {
+  ADD_GAME: "add_game",
+};
+
 export const FabOptions: IActionProps[] = [
   {
-    name: "add_game",
+    name: ACTION_NAMES.ADD_GAME,
     text: "Dodaj grę do kolekcji",
     icon: <PlusCircle size="$2" color="white" />,
   },
