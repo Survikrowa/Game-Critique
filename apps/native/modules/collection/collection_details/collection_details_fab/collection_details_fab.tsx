201e49aac7fc7085d438c0951465729534b8f78b
survikrowa <szymonhrabia60@gmail.com>
2024-02-04 00:56:03 +0100

    feat(native): Move action names to dictionary

--- apps/native/modules/collection/collection_details/collection_details_fab/collection_details_fab.tsx
+++ apps/native/modules/collection/collection_details/collection_details_fab/collection_details_fab.tsx
@@ -1,14 +1,14 @@
 import { router } from "expo-router";
 import { FloatingAction } from "react-native-floating-action";
 
-import { FabOptions } from "./fab_options";
+import { ACTION_NAMES, FabOptions } from "./fab_options";
 
 export const CollectionDetailsFab = () => {
   return (
     <FloatingAction
       actions={FabOptions}
       onPressItem={(name) => {
-        if (name === "add_game") {
+        if (name === ACTION_NAMES.ADD_GAME) {
           router.push("search");
         }
       }}
