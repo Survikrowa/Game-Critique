a201530b89b64bfae3432c2de58c1c7f805269ee
survikrowa <szymonhrabia60@gmail.com>
2024-02-08 20:57:30 +0100

    feat(native): Added empty collection screen

--- apps/native/modules/collection/collection_details/collection_details_fab/collection_details_fab.tsx
+++ apps/native/modules/collection/collection_details/collection_details_fab/collection_details_fab.tsx
@@ -1,17 +1,20 @@
 import { router } from "expo-router";
 import { FloatingAction } from "react-native-floating-action";
+import { XStack } from "tamagui";
 
 import { ACTION_NAMES, FabOptions } from "./fab_options";
 
 export const CollectionDetailsFab = () => {
   return (
-    <FloatingAction
-      actions={FabOptions}
-      onPressItem={(name) => {
-        if (name === ACTION_NAMES.ADD_GAME) {
-          router.push("search");
-        }
-      }}
-    />
+    <XStack position="absolute" bottom={10} right={10}>
+      <FloatingAction
+        actions={FabOptions}
+        onPressItem={(name) => {
+          if (name === ACTION_NAMES.ADD_GAME) {
+            router.push("search");
+          }
+        }}
+      />
+    </XStack>
   );
 };
