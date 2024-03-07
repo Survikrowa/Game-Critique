9ce68574429e6c958c48c5d632c5324e27956e23
survikrowa <szymonhrabia60@gmail.com>
2024-03-08 00:52:03 +0100

    fix(native): missing scroll/fab position

--- apps/native/modules/screens/games/games_screen.tsx
+++ apps/native/modules/screens/games/games_screen.tsx
@@ -1,11 +1,13 @@
+import { ScrollView } from "tamagui";
+
 import { GamesStatusCategoriesFab } from "./games_status_categories_fab/games_status_categories_fab";
 import { GamesStatusCategoriesTabs } from "../../games/games_status_categories_tabs/games_status_categories_tabs";
 
 export const GamesScreen = () => {
   return (
-    <>
+    <ScrollView maxHeight="99%" position="relative">
       <GamesStatusCategoriesTabs />
       <GamesStatusCategoriesFab />
-    </>
+    </ScrollView>
   );
 };
