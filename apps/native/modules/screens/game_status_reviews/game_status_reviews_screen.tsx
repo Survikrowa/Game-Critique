48d9a3fe2246436927b331640eba09e7e8f83612
survikrowa <szymonhrabia60@gmail.com>
2024-03-08 22:00:07 +0100

    feat(api/native): Added user game score to the game reviews screen

--- apps/native/modules/screens/game_status_reviews/game_status_reviews_screen.tsx
+++ apps/native/modules/screens/game_status_reviews/game_status_reviews_screen.tsx
@@ -5,6 +5,7 @@ import { ScrollView, Separator, Spinner, XStack, YStack } from "tamagui";
 import { useGameStatusReviewStore } from "./use_game_status_review_store/use_game_status_review_store";
 import { Text } from "../../../ui/typography/text";
 import { useFriendsGameReviews } from "../../games/user_game_status/user_game_status_friends_reviews/use_friends_game_reviews/use_friends_game_reviews";
+import { parseScore } from "../../games/user_game_status/user_game_status_sections/user_game_status_score_section/user_game_status_score_section";
 import { UserAvatar } from "../../profile/user_avatar/user_avatar";
 import { truncateString } from "../../strings/truncate_string";
 
@@ -70,10 +71,18 @@ export const GameStatusReviewsScreen = ({
                     size="$3"
                     avatarUrl={review.profile?.avatarUrl || ""}
                   />
-                  <YStack>
-                    <Text size="medium" weight="bold" color="primary">
-                      {review.profile?.name}
-                    </Text>
+                  <YStack gap={8}>
+                    <XStack gap={8} justifyContent="space-between">
+                      <Text size="medium" weight="bold" color="primary">
+                        {truncateString(review.profile?.name || "", 20)}
+                      </Text>
+                      {review.score && (
+                        <Text size="medium" weight="bold" color="primary">
+                          Ocena gry {parseScore(review.score)}
+                        </Text>
+                      )}
+                    </XStack>
+
                     <Text size="small" color="primary" weight="normal">
                       {truncateString(review.review || "", 40)}
                     </Text>
