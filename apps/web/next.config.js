585b59d8fecd650be579ec6268578e3830cc86ad
survikrowa <szymonhrabia60@gmail.com>
2024-07-03 19:58:51 +0200

    feat(core): cleanup packages

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,25 +1,11 @@
-const path = require("path");
-
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
   experimental: {
-    // this includes files from the monorepo base two directories up
-    outputFileTracingRoot: path.join(__dirname, "../../"),
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      // Transform all direct `react-native` imports to `react-native-web`
-      "react-native$": "react-native-web",
-    };
-    config.resolve.extensions = [
-      ".web.js",
-      ".web.jsx",
-      ".web.ts",
-      ".web.tsx",
-      ...config.resolve.extensions,
-    ];
-    return config;
+    ppr: true,
+    reactCompiler: true,
+    typedRoutes: true,
   },
 };
+
+module.exports = nextConfig;
