3648c3bae8ec21c3d65ca47cda632cca94e1ec21
survikrowa <szymonhrabia60@gmail.com>
2024-10-02 16:17:22 +0200

    feat(api): increase app timeout

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(process.env.PORT || 3000);
   const server = app.getHttpServer();
   server.setTimeout(600_000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
