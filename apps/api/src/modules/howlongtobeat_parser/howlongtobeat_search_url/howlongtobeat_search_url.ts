f22bb25b92ebb684a106de497621921826e257ec
Szymon Kozioł <szymonkoziol3@gmail.com>
2024-10-03 21:30:04 +0200

    fix: puppeteer in docker

--- apps/api/src/modules/howlongtobeat_parser/howlongtobeat_search_url/howlongtobeat_search_url.ts
+++ apps/api/src/modules/howlongtobeat_parser/howlongtobeat_search_url/howlongtobeat_search_url.ts
@@ -4,12 +4,28 @@ import puppeteerStealth from 'puppeteer-extra-plugin-stealth';
 interface HowLongToBeatSearchUrlFields {
   searchHash: string;
 }
+
 puppeteer.use(puppeteerStealth());
+
 export class HowLongToBeatSearchUrl implements HowLongToBeatSearchUrlFields {
   public searchHash: string = '';
 
   async updateSearchHash() {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: [
+        '--disable-setuid-sandbox',
+        '--no-sandbox',
+        '--single-process',
+        '--no-zygote',
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+      ],
+      executablePath:
+        process.env.NODE_ENV === 'production'
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
+    });
     const page = await browser.newPage();
 
     await page.setRequestInterception(true);
