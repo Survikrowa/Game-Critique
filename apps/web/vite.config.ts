b8698dbbdd63c85223b19ceb0b1e55b110bdcdaa
survikrowa <szymonhrabia60@gmail.com>
2024-10-25 22:57:11 +0200

    feat(web): tailwind config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
+import * as path from "node:path";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
