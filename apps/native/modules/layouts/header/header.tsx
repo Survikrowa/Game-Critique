dd70b24587a0cc6b305b77897e1cb787e6f51915
survikrowa <szymonhrabia60@gmail.com>
2025-05-08 12:52:42 +0200

    fix(native): missing auth path

--- apps/native/modules/layouts/header/header.tsx
+++ apps/native/modules/layouts/header/header.tsx
@@ -1,14 +1,18 @@
 import { Link } from "expo-router";
 import { View } from "react-native";
+import { useAuth0 } from "react-native-auth0";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { XStack } from "tamagui";
 
 import { SearchButton } from "./search/search_button";
+import { UserProfileButton } from "./user_profile_button";
 import AppLogo from "../../../assets/logo/logo_app.svg";
 import { Text } from "../../../ui/typography/text";
 
 export const Header = () => {
   const insets = useSafeAreaInsets();
+  const { user } = useAuth0();
+
   return (
     <>
       <View style={{ paddingTop: insets.top }} />
@@ -19,6 +23,7 @@ export const Header = () => {
         alignItems="center"
         justifyContent="space-between"
       >
+        {!user && <UserProfileButton />}
         <Link href="/home">
           <Text size="medium" color="primary" weight="semiBold">
             <AppLogo width={48} height={48} style={{ color: "white" }} />
--- apps/native/modules/layouts/header/user_profile_button.tsx
+++ apps/native/modules/layouts/header/user_profile_button.tsx
@@ -1,16 +1,13 @@
-import { User } from "@tamagui/lucide-icons";
+import { LogIn } from "@tamagui/lucide-icons";
 import { router } from "expo-router";
-import { useAuth0 } from "react-native-auth0";
 
 import { ButtonWithIcon } from "../../../ui/forms/button_icon";
 
 export const UserProfileButton = () => {
-  const { user } = useAuth0();
-  const redirectTo = user ? "user" : "auth";
   return (
     <ButtonWithIcon
-      onPress={() => router.push(redirectTo)}
-      icon={<User color="$purple1" size="$2" />}
+      onPress={() => router.push("auth")}
+      icon={<LogIn color="$purple1" size="$2" />}
       backgroundColor="transparent"
       padding={4}
     />
